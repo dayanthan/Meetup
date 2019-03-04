f36c5a5ca6a4d9e88bbc15bd6eb30d640b4accb3
dayanthan <daya.naukri@gmail.com>
2019-03-04 10:17:35 +0530

    removed unwanted codes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,22 +17,3 @@
 //= require turbolinks
 //= require_tree .
 
-
-
-function eatFood() {
-alert('Form has been submitted');
-// var form = $(this);
-//     var url = form.attr('action');
-
-    $.ajax({
-           type: "POST",
-           url: "add_comments",
-           data: form.serialize(), // serializes the form's elements.
-           success: function(data)
-           {
-               alert(data); // show response from the php script.
-           }
-         });
-preventDefault();
-}
-
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,10 +1,13 @@
 module CommentsHelper
+
 	def all_comments(id)
 		@comments=Comment.where("post_id=?",id)
 	end
+
 	def find_user(comment)
 		@user=User.find_by_id(comment.user_id)
 	end
+	
 	def total_replies(comment)
 		@all_replies=ReplyComment.where("comment_id=?", comment.id)
 	end
